23bfd76c850e9d85d076e3730cc81236e5ccdf0f
Peter Bui <peter@pbwebdev.com.au>
2022-06-29 22:13:07 +1000

    Update archive-podcasts.php
    category tweaks

--- archive-podcasts.php
+++ archive-podcasts.php
@@ -12,7 +12,7 @@ get_header();
 ?>
 
 
-<div class="archive-header pt-2">
+<div class="archive-header py-5">
     <div class="container">
         <div class="row justify-content-md-center">
             <div class="col-md-8 text-center">
@@ -23,7 +23,7 @@ get_header();
     </div>
 </div>
 
-    <main class="category <?php echo cardanopress_bootstrap_class( 'content-full' ); ?>">
+    <main class="podcast-category <?php echo cardanopress_bootstrap_class( 'content-full' ); ?>">
 
         <div class="latest section">
             <div class="container">
@@ -49,11 +49,10 @@ get_header();
 
                                                     <div class="row metadata">
                                                         <div class="episode-tags col">
-    <span class="badge rounded-pill text-bg-primary"><?php
-        echo get_the_term_list( $post->ID, 'ptag', '<ul class="styles"><li>', ',</li><li>', '</li></ul>' );
+    <?php echo get_the_term_list( $post->ID, 'ptag', '<span class="badge rounded-pill text-bg-primary">', ',</span> <span class="badge rounded-pill text-bg-primary">', '</span>' );
         ?></span>
                                                         </div>
-                                                        <div class="episode-date col text-end"> <?php the_time('F jS, Y') ?></div>
+                                                        <div class="episode-date col text-end"> <?php the_time('d M, Y') ?></div>
                                                     </div>
 
 
@@ -68,7 +67,7 @@ get_header();
                                                     </div>
                                                     <a href="<?php the_permalink() ?>" class="btn btn-primary"  rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
                                                         <span class="fa fa-play"></span> Play episode
-                                                    </a> <span class="btn-secondary btn"><span class="fa fa-comment"></span> <?php printf( _n( '%d comment', '%d comments', get_comments_number(), 'learncardano' ), get_comments_number() ); ?></span>
+                                                    </a> <a  href="<?php the_permalink() ?>#comments"  class="btn-secondary btn"><span class="fa fa-comment"></span> <?php printf( _n( '%d comment', '%d comments', get_comments_number(), 'learncardano' ), get_comments_number() ); ?></a>
 
                                                 </div>
                                             </div>
