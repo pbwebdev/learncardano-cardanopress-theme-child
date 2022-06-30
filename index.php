49132943b5f48884338c2edc2f11e22a83c8655a
Peter Bui <peter@pbwebdev.com.au>
2022-06-30 15:52:57 +1000

    Create index.php
    global theme file

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * The main template file
+ *
+ * @package CardanoPress Bootstrap
+ * @since 0.1.0
+ */
+
+get_header();
+
+?>
+<?php get_template_part( 'template-parts/global', 'banner' ); ?>
+
+<div class="container">
+	<div class="content-sidebar row">
+		<main class="<?php echo cardanopress_child_bootstrap_class( 'content' ); ?> py-5">
+
+			<?php if ( have_posts() ) : ?>
+				<?php while ( have_posts() ) : ?>
+					<?php the_post(); ?>
+
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+						<?php if ( has_post_thumbnail() ) : ?>
+							<div class="entry-featured">
+								<?php the_post_thumbnail(); ?>
+							</div>
+						<?php endif; ?>
+
+						<div class="entry-wrap">
+							<header class="entry-header">
+								<h2 class="entry-title"><a href="<?php the_permalink(); ?>">
+									<?php the_title(); ?>
+								</a></h2>
+
+<!--								<div class="entry-meta">-->
+<!--									<time>--><?php //the_date(); ?><!--</time>-->
+<!--									<span>--><?php //the_author(); ?><!--</span>-->
+<!--								</div>-->
+							</header>
+
+							<div class="entry-content">
+								<?php the_excerpt(); ?>
+							</div>
+
+							<footer class="entry-footer">
+								<?php the_tags(); ?>
+							</footer>
+						</div>
+					</article><!-- #post-<?php the_ID(); ?> -->
+
+				<?php endwhile; ?>
+
+				<?php the_posts_pagination(); ?>
+
+			<?php endif; ?>
+
+		</main><!-- .content -->
+
+		<?php get_sidebar(); ?>
+
+	</div><!-- .content-sidebar -->
+</div><!-- .container -->
+
+<?php
+
+get_footer();
