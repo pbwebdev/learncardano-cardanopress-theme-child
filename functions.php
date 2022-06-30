126df20a7e86e9af904b73a66cd90c319fe06621
Peter Bui <peter@pbwebdev.com.au>
2022-06-30 15:52:40 +1000

    Update functions.php
    custom dynamic classes

--- functions.php
+++ functions.php
@@ -175,3 +175,47 @@ if( function_exists('acf_add_local_field_group') ):
     ));
 
 endif;
+
+
+function cardanopress_child_bootstrap_class( string $key ): string {
+    $classes = array(
+        'content' => array(
+            'content',
+        ),
+
+        'content-active-sidebar' => array(
+            'col-sm-12',
+            'col-md-7',
+            'col-lg-8',
+        ),
+
+        'content-inactive-sidebar' => array(
+            'inactive-sidebar',
+            'w-100',
+        ),
+
+        'sidebar' => array(
+            'sidebar',
+            'py-5',
+            'col-sm-12',
+            'col-md-5',
+            'col-lg-4',
+        ),
+    );
+
+    $class_string = '';
+
+    if ( 'content' === $key ) {
+        $exta_class_key     = is_active_sidebar( 'sidebar' ) ? 'content-active-sidebar' : 'content-inactive-sidebar';
+        $classes['content'] = array_merge( $classes['content'], $classes[ $exta_class_key ] );
+    } elseif ( 'content-full' === $key ) {
+        $key                = 'content';
+        $classes['content'] = array_merge( $classes['content'], array( 'w-100' ) );
+    }
+
+    if ( array_key_exists( $key, $classes ) ) {
+        $class_string = implode( ' ', $classes[ $key ] );
+    }
+
+    return $class_string;
+}
