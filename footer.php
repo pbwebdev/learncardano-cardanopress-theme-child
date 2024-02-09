140e7e5ef83492960a89c899f3c6484c39044a9e
pbwebdev <peter@pbwebdev.com.au>
2024-02-09 17:01:20 +1000

    remove MC code

--- footer.php
+++ footer.php
@@ -129,7 +129,6 @@ $theme = wp_get_theme( CARDANOPRESS_BOOTSTRAP_THEME_BASE );
 
     gtag('config', 'G-X7VNV35VXF');
 </script>
-<script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/4533228f93a76b04355cd71bd/eabdef0340c6dd62a81ca526a.js");</script>	</body>
 <!--<?php echo $post_type; ?>-->
 </html>
 
