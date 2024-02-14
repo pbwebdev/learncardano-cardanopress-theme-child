d94b198a7fab632bd897936235e012bf51cf4418
pbwebdev <peter@pbwebdev.com.au>
2024-02-14 10:39:28 +1000

    remove image on archive

--- archive-podcasts.php
+++ archive-podcasts.php
@@ -40,13 +40,7 @@ get_header();
                                         <div class="episode-card">
 
                                             <div class="row">
-                                                <div class="col-md-3 episode-image align-self-start">
-                                                    <?php if ( has_post_thumbnail() ) : ?>
-                                                        <?php the_post_thumbnail(); ?>
-                                                    <?php endif; ?>
-                                                </div>
-                                                <div class="col-md-8 offset-md-1 episode-details">
-
+                                                <div class="col-12 episode-details">
                                                     <div class="row metadata">
                                                         <div class="episode-tags col">
     <?php echo get_the_term_list( $post->ID, 'ptag', '<span class="badge rounded-pill text-bg-primary">', ',</span> <span class="badge rounded-pill text-bg-primary">', '</span>' );
