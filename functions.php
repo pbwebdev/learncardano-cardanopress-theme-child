f7a10243f500f87ed06210d2d5c63273002fec8c
karlo <karlo@pbwebdev.com.au>
2025-04-03 16:59:07 +0800

    lock content if not login for podcasts

--- functions.php
+++ functions.php
@@ -1,239 +1,316 @@
 <?php
 
-add_theme_support( 'responsive-embeds' );
+add_theme_support('responsive-embeds');
 
-if ( class_exists( 'ThemePlate' ) ) :
-	ThemePlate( array(
+if (class_exists('ThemePlate')) :
+	ThemePlate(array(
 		'title' => 'Learn Cardano',
 		'key'   => 'lc',
-	) );
+	));
 
-	
+
 	require_once 'setup/post-types.php';
 endif;
 
 
-if ( ! function_exists( 'lc_widgets_init' ) ) {
-    function lc_widgets_init() {
-        register_sidebar( array(
-            'id'            => 'FooterCol1',
-            'name'          => __( 'Footer Col 1', 'learncardano' ),
-            'description'   => __( 'Footer Col 1', 'learncardano' ),
-            'before_title'  => '<h3 class="widget-title">',
-            'after_title'   => '</h3>',
-            'before_widget' => '<div class="section %2$s">',
-            'after_widget'  => '</div>'
-        ) );
-        register_sidebar( array(
-            'id'            => 'FooterCol2',
-            'name'          => __( 'Footer Col 2', 'learncardano' ),
-            'description'   => __( 'Footer Col 2', 'learncardano' ),
-            'before_title'  => '<h3 class="widget-title">',
-            'after_title'   => '</h3>',
-            'before_widget' => '<section class="widget %2$s">',
-            'after_widget'  => '</section>'
-        ) );
-        register_sidebar( array(
-            'id'            => 'FooterCol3',
-            'name'          => __( 'Footer Col 3', 'learncardano' ),
-            'description'   => __( 'Footer Col 3', 'learncardano' ),
-            'before_title'  => '<h3 class="widget-title">',
-            'after_title'   => '</h3>',
-            'before_widget' => '<section class="widget %2$s">',
-            'after_widget'  => '</section>'
-        ) );
-
-    }
-    add_action( 'widgets_init', 'lc_widgets_init' );
+if (! function_exists('lc_widgets_init')) {
+	function lc_widgets_init()
+	{
+		register_sidebar(array(
+			'id'            => 'FooterCol1',
+			'name'          => __('Footer Col 1', 'learncardano'),
+			'description'   => __('Footer Col 1', 'learncardano'),
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
+			'before_widget' => '<div class="section %2$s">',
+			'after_widget'  => '</div>'
+		));
+		register_sidebar(array(
+			'id'            => 'FooterCol2',
+			'name'          => __('Footer Col 2', 'learncardano'),
+			'description'   => __('Footer Col 2', 'learncardano'),
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
+			'before_widget' => '<section class="widget %2$s">',
+			'after_widget'  => '</section>'
+		));
+		register_sidebar(array(
+			'id'            => 'FooterCol3',
+			'name'          => __('Footer Col 3', 'learncardano'),
+			'description'   => __('Footer Col 3', 'learncardano'),
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
+			'before_widget' => '<section class="widget %2$s">',
+			'after_widget'  => '</section>'
+		));
+	}
+	add_action('widgets_init', 'lc_widgets_init');
 }
 
 
-if( function_exists('acf_add_local_field_group') ):
+if (function_exists('acf_add_local_field_group')):
 
-    add_action( 'acf/include_fields', function() {
-	if ( ! function_exists( 'acf_add_local_field_group' ) ) {
-		return;
-	}
+	add_action('acf/include_fields', function () {
+		if (! function_exists('acf_add_local_field_group')) {
+			return;
+		}
 
-	acf_add_local_field_group( array(
-	'key' => 'group_62ba92870d1cc',
-	'title' => 'Podcast Episode Meta',
-	'fields' => array(
-		array(
-			'key' => 'field_62ba92a41d393',
-			'label' => 'Embed Code',
-			'name' => 'spreaker_code',
-			'aria-label' => '',
-			'type' => 'text',
-			'instructions' => 'Inline code for embed
+		acf_add_local_field_group(array(
+			'key' => 'group_62ba92870d1cc',
+			'title' => 'Podcast Episode Meta',
+			'fields' => array(
+				array(
+					'key' => 'field_62ba92a41d393',
+					'label' => 'Embed Code',
+					'name' => 'spreaker_code',
+					'aria-label' => '',
+					'type' => 'text',
+					'instructions' => 'Inline code for embed
 
 <iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/3Qf2wWqpPwecaiCNOQYxwH?utm_source=generator" width="100%" height="100" frameBorder="0" allowfullscreen="" allow="clipboard-write; encrypted-media; fullscreen" loading="lazy"></iframe>',
-			'required' => 1,
-			'conditional_logic' => 0,
-			'wrapper' => array(
-				'width' => '',
-				'class' => '',
-				'id' => '',
+					'required' => 1,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/3Qf2wWqpPwecaiCNOQYxwH?utm_source=generator" width="100%" height="100" frameBorder="0" allowfullscreen="" allow="clipboard-write; encrypted-media; fullscreen" loading="lazy"></iframe>',
+					'maxlength' => '',
+					'placeholder' => '',
+					'prepend' => '',
+					'append' => '',
+				),
+				array(
+					'key' => 'field_62ba92de1d394',
+					'label' => 'YouTube Video',
+					'name' => 'youtube_video',
+					'aria-label' => '',
+					'type' => 'text',
+					'instructions' => 'Just the video ID :)',
+					'required' => 1,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '',
+					'maxlength' => '',
+					'placeholder' => '',
+					'prepend' => '',
+					'append' => '',
+				),
+				array(
+					'key' => 'field_64a7e9539f809',
+					'label' => 'Text transcript',
+					'name' => 'text_transcript',
+					'aria-label' => '',
+					'type' => 'wysiwyg',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '',
+					'maxlength' => '',
+					'rows' => '',
+					'placeholder' => '',
+					'new_lines' => '',
+				),
 			),
-			'default_value' => '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/3Qf2wWqpPwecaiCNOQYxwH?utm_source=generator" width="100%" height="100" frameBorder="0" allowfullscreen="" allow="clipboard-write; encrypted-media; fullscreen" loading="lazy"></iframe>',
-			'maxlength' => '',
-			'placeholder' => '',
-			'prepend' => '',
-			'append' => '',
-		),
-		array(
-			'key' => 'field_62ba92de1d394',
-			'label' => 'YouTube Video',
-			'name' => 'youtube_video',
-			'aria-label' => '',
-			'type' => 'text',
-			'instructions' => 'Just the video ID :)',
-			'required' => 1,
-			'conditional_logic' => 0,
-			'wrapper' => array(
-				'width' => '',
-				'class' => '',
-				'id' => '',
+			'location' => array(
+				array(
+					array(
+						'param' => 'post_type',
+						'operator' => '==',
+						'value' => 'podcasts',
+					),
+				),
 			),
-			'default_value' => '',
-			'maxlength' => '',
-			'placeholder' => '',
-			'prepend' => '',
-			'append' => '',
+			'menu_order' => 0,
+			'position' => 'normal',
+			'style' => 'default',
+			'label_placement' => 'top',
+			'instruction_placement' => 'label',
+			'hide_on_screen' => '',
+			'active' => true,
+			'description' => '',
+			'show_in_rest' => 0,
+		));
+	});
+
+endif;
+
+
+function cardanopress_child_bootstrap_class(string $key): string
+{
+	$classes = array(
+		'content' => array(
+			'content',
 		),
-		array(
-			'key' => 'field_64a7e9539f809',
-			'label' => 'Text transcript',
-			'name' => 'text_transcript',
-			'aria-label' => '',
-			'type' => 'wysiwyg',
-			'instructions' => '',
-			'required' => 0,
-			'conditional_logic' => 0,
-			'wrapper' => array(
-				'width' => '',
-				'class' => '',
-				'id' => '',
-			),
-			'default_value' => '',
-			'maxlength' => '',
-			'rows' => '',
-			'placeholder' => '',
-			'new_lines' => '',
+
+		'content-active-sidebar' => array(
+			'col-sm-12',
+			'col-lg-7',
+			'py-3',
 		),
-	),
-	'location' => array(
-		array(
-			array(
-				'param' => 'post_type',
-				'operator' => '==',
-				'value' => 'podcasts',
-			),
+
+		'content-inactive-sidebar' => array(
+			'inactive-sidebar',
+			'w-100',
+			'py-3',
 		),
-	),
-	'menu_order' => 0,
-	'position' => 'normal',
-	'style' => 'default',
-	'label_placement' => 'top',
-	'instruction_placement' => 'label',
-	'hide_on_screen' => '',
-	'active' => true,
-	'description' => '',
-	'show_in_rest' => 0,
-) );
-} );
-    
-    endif;		
 
+		'sidebar' => array(
+			'sidebar',
+			'py-3',
+			'col-sm-12',
+			'col-lg-4',
+			'offset-lg-1',
+		),
+	);
 
-function cardanopress_child_bootstrap_class( string $key ): string {
-    $classes = array(
-        'content' => array(
-            'content',
-        ),
+	$class_string = '';
 
-        'content-active-sidebar' => array(
-            'col-sm-12',
-            'col-lg-7',
-            'py-3',
-        ),
+	if ('content' === $key) {
+		$exta_class_key     = is_active_sidebar('sidebar') ? 'content-active-sidebar' : 'content-inactive-sidebar';
+		$classes['content'] = array_merge($classes['content'], $classes[$exta_class_key]);
+	} elseif ('content-full' === $key) {
+		$key                = 'content';
+		$classes['content'] = array_merge($classes['content'], array('w-100'));
+	}
 
-        'content-inactive-sidebar' => array(
-            'inactive-sidebar',
-            'w-100',
-            'py-3',
-        ),
+	if (array_key_exists($key, $classes)) {
+		$class_string = implode(' ', $classes[$key]);
+	}
 
-        'sidebar' => array(
-            'sidebar',
-            'py-3',
-            'col-sm-12',
-            'col-lg-4',
-            'offset-lg-1',
-        ),
-    );
-
-    $class_string = '';
-
-    if ( 'content' === $key ) {
-        $exta_class_key     = is_active_sidebar( 'sidebar' ) ? 'content-active-sidebar' : 'content-inactive-sidebar';
-        $classes['content'] = array_merge( $classes['content'], $classes[ $exta_class_key ] );
-    } elseif ( 'content-full' === $key ) {
-        $key                = 'content';
-        $classes['content'] = array_merge( $classes['content'], array( 'w-100' ) );
-    }
-
-    if ( array_key_exists( $key, $classes ) ) {
-        $class_string = implode( ' ', $classes[ $key ] );
-    }
-
-    return $class_string;
+	return $class_string;
 }
 
-add_filter( 'comment_form_defaults', function( $fields ) {
-	
-	if ( !is_user_logged_in() ) :
-	
-    $fields['must_log_in'] = sprintf( 
-            __(do_shortcode('[learndash_login login_label=" You must be logged in to post a comment."]')), wp_registration_url(), wp_login_url( apply_filters( 'the_permalink', get_permalink() ) )   
-    );
+add_filter('comment_form_defaults', function ($fields) {
 
-    return $fields;
-	
+	if (!is_user_logged_in()) :
+
+		$fields['must_log_in'] = sprintf(
+			__(do_shortcode('[learndash_login login_label=" You must be logged in to post a comment."]')),
+			wp_registration_url(),
+			wp_login_url(apply_filters('the_permalink', get_permalink()))
+		);
+
+		return $fields;
+
 	endif;
 });
 
-add_filter( 'get_comment_author', function (  $author, $comment_ID, $comment )
-{
-    // Get the current post object
-    $post = get_post();
+add_filter('get_comment_author', function ($author, $comment_ID, $comment) {
+	// Get the current post object
+	$post = get_post();
 
-    // The user ids are the same, lets append our extra text
-	if(strlen($author) >= 10) {
-		$author = substr($author, 0, 5)."...";
+	// The user ids are the same, lets append our extra text
+	if (strlen($author) >= 10) {
+		$author = substr($author, 0, 5) . "...";
 	} else {
 		$author = $author;
 	}
-    
-    return $author;
-}, 10, 3 );
+
+	return $author;
+}, 10, 3);
 
 add_action('init', 'enable_proposal_comments');
-function enable_proposal_comments() {
-	add_post_type_support( 'proposal', 'comments' );
+function enable_proposal_comments()
+{
+	add_post_type_support('proposal', 'comments');
 }
 
 
-function wpb_modify_jquery() {
-    // Check if front-end is being viewed
-    if (!is_admin()) {
-        // Remove default WordPress jQuery
-        wp_deregister_script('jquery');
-        // Register new jQuery script via Google Library
-        wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js', false, '3.6.0');
-        // Enqueue the script
-        wp_enqueue_script('jquery');
-    }
+function wpb_modify_jquery()
+{
+	// Check if front-end is being viewed
+	if (!is_admin()) {
+		// Remove default WordPress jQuery
+		wp_deregister_script('jquery');
+		// Register new jQuery script via Google Library
+		wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js', false, '3.6.0');
+		// Enqueue the script
+		wp_enqueue_script('jquery');
+	}
 }
 // Execute the action when WordPress is initialized
 add_action('init', 'wpb_modify_jquery');
+
+
+function cardano_restrict_selected_content($content)
+{
+
+	$post_filter = get_field('post_type', 'option');
+	$post_type = get_post_type();
+
+	if (!in_array($post_type, $post_filter) || is_admin() || is_user_logged_in()) {
+		return $content;
+	}
+
+	if (!in_the_loop() || !is_main_query()) {
+		return $content;
+	}
+
+	if (strpos($content, '<!--more-->') !== false) {
+		list($teaser, $locked_content) = explode('<!--more-->', $content, 2);
+
+		$cta = '
+        <div class="subscriber-lock-box py-3">
+            <strong>This content is for subscribers only.</strong><br>
+            Log in to continue reading or access exclusive media.<br>
+            <a href="#navbarSupportedContent" class="sub-btn">Log in</a>
+        </div>';
+
+		return apply_filters('the_content', $teaser) . $cta;
+	}
+
+	return wp_trim_words($content, 250, '...') . '
+        <div class="subscriber-lock-box py-3">
+            <strong>This content is for subscribers only.</strong><br>
+            <a href="#navbarSupportedContent" class="sub-btn">Log in</a>
+        </div>';
+}
+add_filter('the_content', 'cardano_restrict_selected_content');
+
+function cardano_acf_locked_field($field_name, $args = [])
+{
+	$defaults = [
+		'post_id'        => get_the_ID(),
+		'more_tag'       => '<!--more-->',
+		'word_limit'     => 250,
+		'css_class'      => 'subscriber-lock-box',
+		'login_url'      => '#navbarSupportedContent',
+		'subscribe_url'  => '/subscribe',
+		'title'          => 'This content is for subscribers only.',
+	];
+	$args = wp_parse_args($args, $defaults);
+
+	$field_value = apply_filters('olrak_acf_locked_field_value', get_field($field_name, $args['post_id']), $field_name, $args);
+
+	if (!$field_value) return;
+
+	if (is_user_logged_in()) {
+		echo '<div>' . wp_kses_post($field_value) . '</div>';
+		return;
+	}
+
+	if (strpos($field_value, $args['more_tag']) !== false) {
+		list($teaser, $rest) = explode($args['more_tag'], $field_value, 2);
+	} else {
+		$teaser = wp_trim_words(strip_tags($field_value), $args['word_limit'], '...');
+	}
+
+	echo '<div class="' . esc_attr($args['css_class']) . '">' . wp_kses_post($teaser) . '</div>';
+
+	echo '
+        <div class="subscriber-lock-box">
+            <strong>' . esc_html($args['title']) . '</strong><br>
+            <a href="' . esc_url($args['login_url']) . '" class="sub-btn">Log in</a>
+        </div>';
+}
--- single-podcasts.php
+++ single-podcasts.php
@@ -13,97 +13,121 @@ get_header();
 
 
 ?>
-<?php if( get_field('youtube_video') ): ?>
+<?php if (get_field('youtube_video')): ?>
     <div id="preloader" style="visibility: hidden; display: none;">
-        <img src="https://i.ytimg.com/vi/<?php the_field('youtube_video'); ?>/hqdefault.jpg" height="1" width="1" style="visibility: hidden; display: none;" alt="" >
+        <img src="https://i.ytimg.com/vi/<?php the_field('youtube_video'); ?>/hqdefault.jpg" height="1" width="1" style="visibility: hidden; display: none;" alt="">
     </div>
 <?php endif; ?>
 
-    <div class="podcast-episode">
-        <main class="content" role="main">
+<div class="podcast-episode">
+    <main class="content" role="main">
 
-            <?php while ( have_posts() ) : the_post(); ?>
+        <?php while (have_posts()) : the_post(); ?>
 
-                <article id="podcast-<?php the_ID(); ?>" class="entry-wrapper">
+            <article id="podcast-<?php the_ID(); ?>" class="entry-wrapper">
 
-                    <div class="content-header py-3">
-                        <div class="container">
-                            <div class="row justify-content-md-center">
-                                <div class="col-md-10 col-sm-12">
-                                    <?php if( get_field('youtube_video') ): ?>
-                                        <div class="youtube-embed col-md-10 mb-2 offset-md-1">
-                                            <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style>
-                                            <div class='embed-container'>
-                                                <iframe src='https://www.youtube.com/embed/<?php the_field('youtube_video'); ?>' frameborder='0' allowfullscreen></iframe>
-                                            </div>
-                                        </div>
-                                    <?php endif; ?>
-                                    <h1 class="entry-title text-center"><?php the_title(); ?></h1>
-                                    <div class="hero-host">
-                                        <div class="hosted-wrapper">
-                                            <!-- <img src="--><?php //echo get_theme_root_uri(); ?><!--/learncardano-cardanopress-theme-child/images/peter-bui-55x55.jpg" alt="Peter Bui - Learn Cardano Podcast" class="hosted-picture float-start">-->
-                                            <p class="hosted-text">Episode by Peter Bui on <span class="episode-date"> <?php the_time('F jS, Y') ?></span></p>
+                <div class="content-header py-3">
+                    <div class="container">
+                        <div class="row justify-content-md-center">
+                            <div class="col-md-10 col-sm-12">
+                                <?php if (get_field('youtube_video')): ?>
+                                    <div class="youtube-embed col-md-10 mb-2 offset-md-1">
+                                        <style>
+                                            .embed-container {
+                                                position: relative;
+                                                padding-bottom: 56.25%;
+                                                height: 0;
+                                                overflow: hidden;
+                                                max-width: 100%;
+                                            }
+
+                                            .embed-container iframe,
+                                            .embed-container object,
+                                            .embed-container embed {
+                                                position: absolute;
+                                                top: 0;
+                                                left: 0;
+                                                width: 100%;
+                                                height: 100%;
+                                            }
+                                        </style>
+                                        <div class='embed-container'>
+                                            <iframe src='https://www.youtube.com/embed/<?php the_field('youtube_video'); ?>' frameborder='0' allowfullscreen></iframe>
                                         </div>
                                     </div>
-                                    <?php //the_field('spreaker_code'); ?>
+                                <?php endif; ?>
+                                <h1 class="entry-title text-center"><?php the_title(); ?></h1>
+                                <div class="hero-host">
+                                    <div class="hosted-wrapper">
+                                        <!-- <img src="--><?php //echo get_theme_root_uri(); 
+                                                            ?><!--/learncardano-cardanopress-theme-child/images/peter-bui-55x55.jpg" alt="Peter Bui - Learn Cardano Podcast" class="hosted-picture float-start">-->
+                                        <p class="hosted-text">Episode by Peter Bui on <span class="episode-date"> <?php the_time('F jS, Y') ?></span></p>
+                                    </div>
                                 </div>
+                                <?php //the_field('spreaker_code'); 
+                                ?>
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <div class="content-body py-4">
-                        <div class="container">
-                            <div class="row justify-content-md-center">
-                                <div class="col-lg-8 col-md-12">
+                <div class="content-body py-4">
+                    <div class="container">
+                        <div class="row justify-content-md-center">
+                            <div class="col-lg-8 col-md-12">
 
-                                    <?php the_content(); ?>
-									
-									<?php if (get_field('text_transcript')) : ?>
-										<h2>Text Transcript</h2>
-										<div id="text_transcript" style="width: 100%; display: block; height: 250px; overflow:auto;">
-											<?php the_field('text_transcript'); ?>
-										</div>
-									<?php endif; ?>
-									
-                                </div>
-								
+                                <?php the_content(); ?>
+
+                                <?php if (get_field('text_transcript')) : ?>
+                                    <h2>Text Transcript</h2>
+                                    <div id="text_transcript" style="width: 100%; display: block; height: 250px; overflow:auto;">
+                                        <?php
+                                            cardano_acf_locked_field('text_transcript', [
+                                                'title' => 'Transcript is for subscribers only.',
+                                            ]);
+                                        ?>
+                                    </div>
+                                <?php endif; ?>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="podcast-comments pt-5 section">
+                    <div class="container">
+                        <div class="row justify-content-md-center">
+                            <div class="col-lg-10 col-md-12">
+                                <h2>Comments</h2>
+                                <?php
+
+                                // If comments are open or we have at least one comment, load up the comment template.
+                                if (comments_open() || get_comments_number()) :
+                                    comments_template();
+                                endif;
+
+                                the_post_navigation(
+                                    array(
+                                        'prev_text' => '<span class="screen-reader-text">' . __('Previous Post', 'learncardano') . '</span><span aria-hidden="true" class="nav-subtitle">' . __('Previous', 'learncardano') . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper"> <<< </span>%title</span>',
+                                        'next_text' => '<span class="screen-reader-text">' . __('Next Post', 'learncardano') . '</span><span aria-hidden="true" class="nav-subtitle">' . __('Next', 'learncardano') . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper"> >>> </span></span>',
+                                    )
+                                );
+
+                                ?>
                             </div>
                         </div>
                     </div>
+                </div>
 
 
-                    <div class="podcast-comments pt-5 section">
-                        <div class="container">
-                            <div class="row justify-content-md-center">
-                                <div class="col-lg-10 col-md-12">
-                                    <h2>Comments</h2>
-                                    <?php
+            </article><!-- #episode-<?php the_ID(); ?> -->
 
-                                    // If comments are open or we have at least one comment, load up the comment template.
-                                    if ( comments_open() || get_comments_number() ) :
-                                        comments_template();
-                                    endif;
+        <?php endwhile; ?>
 
-                                    the_post_navigation(
-                                        array(
-                                            'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'learncardano' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'learncardano' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper"> <<< </span>%title</span>',
-                                            'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'learncardano' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'learncardano' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper"> >>> </span></span>',
-                                        )
-                                    );
+    </main><!-- .content -->
 
-                                    ?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-
-                </article><!-- #episode-<?php the_ID(); ?> -->
-
-            <?php endwhile; ?>
-
-        </main><!-- .content -->
-
-    </div><!-- .content-sidebar.container -->
+</div><!-- .content-sidebar.container -->
 
 <?php get_footer();
