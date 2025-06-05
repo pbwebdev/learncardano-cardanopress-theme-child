071c91978ee0c23384514a349ac436d6b0b6b523
Pete <peter@pbwebdev.com.au>
2025-06-06 08:27:05 +1000

    adding in footer player

--- footer.php
+++ footer.php
@@ -109,17 +109,17 @@ $theme = wp_get_theme( CARDANOPRESS_BOOTSTRAP_THEME_BASE );
             </div>
         </div>
 
-		<footer class="site-footer py-2 bg-dark text-light">
-			<div class="container">
-				<div class="row">
-                   <div class="copyright text-center">
-                        <small>Copyright &copy; <?php echo esc_html( gmdate( 'Y' ) ); ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="text-light"><?php bloginfo( 'name' ); ?></a></small> -
-                        <span class="small">Powered by <a href="<?php echo esc_url( $theme->get( 'AuthorURI' ) ); ?>" class="text-light"><?php echo esc_html( $theme->get( 'Author' ) ); ?></a>.</span>
-                    </div>
-                </div>
-			</div>
-		</footer>
-<!-- .site-footer -->
+	<footer class="site-footer pt-2 bg-dark text-light" style="padding-bottom:70px;">
+		<div class="container">
+			<div class="row">
+        		        <div class="copyright text-center">
+                			<small>Copyright &copy; <?php echo esc_html( gmdate( 'Y' ) ); ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="text-light"><?php bloginfo( 'name' ); ?></a></small> -
+                        		<span class="small">Powered by <a href="<?php echo esc_url( $theme->get( 'AuthorURI' ) ); ?>" class="text-light"><?php echo esc_html( $theme->get( 'Author' ) ); ?></a>.</span>
+                    		</div>
+                	</div>
+		</div>
+	</footer>
+	<iframe src='https://widget.spreaker.com/player?show_id=4856207&theme=dark&playlist=show&playlist-continuous=true&chapters-image=true' width='100%' height='50px' title='Learn Cardano Podcast' frameborder='0' class="fixed-bottom" ></iframe>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
